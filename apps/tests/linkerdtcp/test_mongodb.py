1c5662912318a74f8dcaa5878c1eca102c2bf957
Robert Roose <rroose@quid.com>
2018-01-11 11:30:46 -0800

    Testing that we can post to mongodb

--- apps/tests/linkerdtcp/test_mongodb.py
+++ apps/tests/linkerdtcp/test_mongodb.py
@@ -1,7 +1,33 @@
+import random
+import string
+
+import pytest
 import requests
+from pymongo import MongoClient
 
 
 def test_http():
     response = requests.get("http://linkerdtcp:7400")
     response.raise_for_status()
     assert "MongoDB" in response.text
+
+
+@pytest.mark.parametrize("host,port", [
+    ("linkerdtcp", 7400), # Test via linkerd-tcp
+    ("mongodb", 27017) # Test direct connection
+])
+def test_connection(host, port):
+    client = MongoClient(host, port)
+    db = client.test_database
+
+    collection_name = "".join(random.choices(string.ascii_lowercase, k=5))
+    collection = db[collection_name]
+
+    post = {
+        "author": "Mike",
+        "text": "My first blog post!",
+        "tags": ["mongodb", "python", "pymongo", random.randint(1, 100)]
+    }
+
+    assert collection.insert_one(post).inserted_id
+    assert collection.find_one() == post
